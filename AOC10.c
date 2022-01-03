a6b235c169a83a633cae989d2b92bd0fb6b64b18
= <=>
2022-01-03 18:10:36 -0500

    cleaned up some things for AOC10

--- AOC10.c
+++ AOC10.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #define debug
 #define NUM_LINES 106
 /**
@@ -17,78 +18,49 @@ typedef enum {lparen = 40, rparen = 41, lbrack = 91, rbrack = 93,
               gts = 62, lts = 60, lbrace = 123, rbrace = 125, NL = '\n' } Symbol;
 
 char *input = NULL;
-// "[({(<(())[]>[[{[]{<()<>>\n"
-// "[(()[<>])]({[<{<<[]>>(\n"
-// "{([(<{}[<>[]}>{[]{[(<()>\n"
-// "(((({<>}<{<{<>}{[]{[]{}\n"
-// "[[<[([]))<([[{}[[()]]]\n"
-// "[{[{({}]{}}([{[{{{}}([]\n"
-// "{<[[]]>}<{[{[{[]{()[[[]\n"
-// "[<(<(<(<{}))><([]([]()\n"
-// "<{([([[(<>()){}]>(<<{{\n"
-// "<{([{{}}[<[[[<>{}]]]>[]]";
-
 unsigned long long tally[NUM_LINES];
-//const char* input = "{([(<{}[<>[]}>{[]{[(<()>";
-
 Symbol sym;
 int lineNum = 0;
 int lineIdx = 0;
-
-void read_file_to_memory() {
-FILE *fp = fopen("./data/aoc10.txt", "r");
-    if (fp != NULL) {
-        /* Go to the end of the file. */
-        if (fseek(fp, 0L, SEEK_END) == 0) {
-            /* Get the size of the file. */
-            long bufsize = ftell(fp);
-            if (bufsize == -1) { /* Error */ }
-
-            /* Allocate our buffer to that size. */
-            input = malloc(sizeof(char) * (bufsize + 1));
-
-            /* Go back to the start of the file. */
-            if (fseek(fp, 0L, SEEK_SET) != 0) { /* Error */ }
-
-            /* Read the entire file into memory. */
-            size_t newLen = fread(input, sizeof(char), bufsize, fp);
-            if ( ferror( fp ) != 0 ) {
-                fputs("Error reading file", stderr);
-            } else {
-                input[newLen++] = '\0'; /* Just to be safe. */
-            }
-        }
-        fclose(fp);
-    }
-}
+clock_t t;
 
 int nextsym(void) {
     char c = input[lineIdx++];
-
     if (c == '\0') {
         unsigned long long int total = 0;
         for (int i = 0; i < NUM_LINES; i++) {
             total += tally[i];
-            printf("Tally at line %d: %lld \n", i, tally[i]);
         }
-        printf("Final Total: %lld \n", total);
+        printf("\nFinal Total: %lld \n", total);
+        
+        t = clock() - t;
+        double time_taken = ((double)t)/CLOCKS_PER_SEC;
+        printf("AOC10 took %f seconds to execute \n", time_taken);
         free(input);
         exit(1);
     }
     if (c == '\n') {
         sym = NL;
-        printf("\n");
-        printf("\n");
-        printf("Line: %d", lineNum++);
-        printf("\n");
-        printf("%c", sym);
+        #ifdef debug
+            printf("\n");
+            printf("\n");
+            printf("Line: %d", lineNum);
+            printf("\n");
+            printf("%c", sym);
+        #endif
+        lineNum++;
         return 1;
     } else if (c != 0) {
         sym = c;
-        printf("%c", sym);
+        #ifdef debug
+            printf("%c", sym);
+        #endif
         return 1;
     } else {
-        printf("Line: %d\n", lineNum++);
+        #ifdef debug
+            printf("Line: %d\n", lineNum);
+        #endif
+        lineNum++;
         return 0;
     }
 }
@@ -125,14 +97,18 @@ int accept(Symbol s) {
 int expect(Symbol s) {
     if (accept(s))
         return 1;
-        
-    printf("Line: %d Pos: %d\n", lineNum, lineIdx);
-    printf("Expected: %c\n", s);
-    printf("Found: %c\n", input[lineIdx - 1]);
+    
+    #ifdef debug
+        printf("Line: %d Pos: %d\n", lineNum, lineIdx);
+        printf("Expected: %c\n", s);
+        printf("Found: %c\n", input[lineIdx - 1]);
+    #endif
     if (tally[lineNum] == 0) {
         tally[lineNum] = get_score((Symbol) input[lineIdx - 1]);
     }
-    printf("\n");
+    #ifdef debug
+        printf("\n");
+    #endif
     return 0;
 }
 
@@ -158,8 +134,36 @@ void parens(void) {
     }
 }
 
+void read_file_to_memory() {
+FILE *fp = fopen("./data/aoc10.txt", "r");
+    if (fp != NULL) {
+        /* Go to the end of the file. */
+        if (fseek(fp, 0L, SEEK_END) == 0) {
+            /* Get the size of the file. */
+            long bufsize = ftell(fp);
+            if (bufsize == -1) { /* Error */ }
+
+            /* Allocate our buffer to that size. */
+            input = malloc(sizeof(char) * (bufsize + 1));
+
+            /* Go back to the start of the file. */
+            if (fseek(fp, 0L, SEEK_SET) != 0) { /* Error */ }
+
+            /* Read the entire file into memory. */
+            size_t newLen = fread(input, sizeof(char), bufsize, fp);
+            if ( ferror( fp ) != 0 ) {
+                fputs("Error reading file", stderr);
+            } else {
+                input[newLen++] = '\0'; /* Just to be safe. */
+            }
+        }
+        fclose(fp);
+    }
+}
+
 int main() {
     read_file_to_memory();
+    t = clock();
     nextsym();
     parens();
 }
